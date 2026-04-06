2ca5bbce1722a8310857248ac19438a5241214a9
ommadure <ommadure7@gmail@gmail.com>
2026-04-06 13:04:25 +0530

    final changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
   build: {
-    sourcemap: 'hidden',
+    sourcemap: "hidden",
   },
   plugins: [
     react({
       babel: {
-        plugins: [
-        ],
+        plugins: [],
       },
     }),
-    tsconfigPaths()
+    tsconfigPaths(),
   ],
-})
+});
